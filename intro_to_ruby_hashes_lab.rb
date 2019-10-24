d2c403264f3a084c00317f9c1f40966b79768fcf
john L <johnleavell@gmail.com>
2019-10-24 15:32:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,34 @@
 def new_hash
   # return an empty hash
+  new_hash = {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  my_hash = {
+    name: "John",
+    age: 39
+  }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer_hash = {
+    :name => "Grace Hopper"
+  }
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  id_hash = {
+    :id => 19
+  }
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  {key => value}
+
 end
 
 def read_from_hash(hash, key)
